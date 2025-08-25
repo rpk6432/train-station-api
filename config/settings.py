a9ad8cdc5ab1368d6e45cb502fab5104d27e9c2b
Roman Peka <pekaromanmail@gmail.com>
2025-08-25 05:36:33 +0300

    refactor: configure project settings

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@%1k#&pcmwi+mw7xehp&h64g-t1k*o9m170*t!b(e7xc&()lb2"
+SECRET_KEY = (
+    "django-insecure-@%1k#&pcmwi+mw7xehp&h64g-t1k*o9m170*t!b(e7xc&()lb2"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "station",
+    "order",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
