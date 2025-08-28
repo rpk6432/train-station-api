cc07ace9000b2867bec169af9c2f6484ba88f447
Roman Peka <pekaromanmail@gmail.com>
2025-08-28 09:50:00 +0300

    feat: add default pagination

--- station/models.py
+++ station/models.py
@@ -11,6 +11,9 @@ class Station(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
 
